2469ee5b4bad7d0868e9c3845df9b4ea0feb0ba6
AleksandrSadouski <sashasadovskyi020507@gmail.com>
2026-07-29 21:13:32 +0300

    feat: add AuthController, LoginRequest, RegisterRequest, ProfileResource

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Profile;
+use App\Http\Resources\ProfilerResource;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+
+
+class AuthController extends Controller
+{
+    public function loginProfile(LoginRequest $request)
+    {
+        $name = $request->input('name');
+        $password = $request->input('password');
+        $profile = Profile::where('name', $name)->first();
+        if (!$profile || !Hash::check($password, $profile->password))
+            {
+                return response()->json(['status' => 'error', 
+                'message' => 'Incorrect data'], 401);
+            }
+        $profile->tokens()->delete();
+        $token = $profile->createToken('auth-token')->plainTextToken;
+        return response()->json(['status' => 'success',
+        'message' => 'Successful login to profile',
+        'data' => new ProfileResource($profile), 
+        'token' => $token]);     
+    }
+    
+    public function registerProfile(RegisterRequest $request)
+    {
+        $name = $request->input('name');
+        $password = Hash::make($request->input('password'));
+        $profile = new Profile();
+        $profile->name = $name;
+        $profile->password = $password;
+        $profile->save();
+        $token = $profile->createToken('auth-token')->plainTextToken;
+        return response()->json(['status' => 'success',
+        'message' => 'Successful profile creation',
+        'data' => new ProfileResource($profile), 
+        'token' => $token]);
+    }
+}
---
+++ app/Http/Resources/ProfileResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProfileResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return ['id_profile' => $this->id,
+        'name' => $this->name,
+        'rating' => $this->rating,
+        'win_total' => $this->win_total,
+        'draw_total' => $this->draw_total,
+        'lose_total' => $this->lose_total];
+    }
+}
