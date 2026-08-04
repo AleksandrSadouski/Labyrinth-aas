a1f884baf2ffb8cbf9ea631eda3a346d1b8c0898
AleksandrSadouski <sashasadovskyi020507@gmail.com>
2026-08-05 01:45:07 +0300

    refactor: UpdateStatsService divided into small methods

--- app/Services/UpdateStatsService.php
+++ app/Services/UpdateStatsService.php
@@ -12,49 +12,58 @@ class UpdateStatsService
         switch($key)
         {
             case 'win':
-                $room->winner_order = $player->player_order;
-                $room->status = 'finished';
-                $room->current_turn = null;
-                $room->save();
-                $profile->game_total++;
-                $profile->win_total++;
-                $profile->rating += 15;
-                $profile->save();
-                $otherPlayer->profile->game_total++;
-                $otherPlayer->profile->lose_total++;
-                $otherPlayer->profile->rating -= 15;
-                $otherPlayer->profile->save();
+                $this->updateRoom($key, $player, $room);
+                $this->updateWinner($profile);
+                $this->updateLoser($otherPlayer->profile);
             break;
             case 'lose':
-                $room->winner_order = $otherPlayer->player_order;
-                $room->status = 'finished';
-                $room->current_turn = null;
-                $room->save();
-                $profile->game_total++;
-                $profile->lose_total++;
-                $profile->rating -= 15;
-                $profile->save();
-                $otherPlayer->profile->game_total++;
-                $otherPlayer->profile->win_total++;
-                $otherPlayer->profile->rating += 15;
-                $otherPlayer->profile->save();
+                $this->updateRoom($key, $otherPlayer, $room);
+                $this->updateWinner($otherPlayer->profile);
+                $this->updateLoser($profile);
             break;
             case 'draw':
-                $room->status = 'finished';
-                $room->draw = true;
-                $room->current_turn = null;
-                $room->save();
-                $profile->game_total++;
-                $profile->draw_total++;
-                $profile->rating += 3;
-                $profile->save();
-                $otherPlayer->profile->game_total++;
-                $otherPlayer->profile->draw_total++;
-                $otherPlayer->profile->rating += 3;
-                $otherPlayer->profile->save();
+                $this->updateRoom($key, $player, $room);
+                $this->updateDrawer($profile);
+                $this->updateDrawer($otherPlayer->profile);
             break;
             default:
             break;
         }
+    }
+
+    public function updateRoom(string $key, Player $player, Room $room): void
+    {
+        $room->status = 'finished';
+        $room->current_turn = null;
+
+        if($key == 'draw')
+        {$room->draw = true;}
+        else $room->winner_order = $player->player_order;
+
+        $room->save();
+    }
+
+    public function updateWinner(Profile $profile): void
+    {
+        $profile->game_total++;
+        $profile->win_total++;
+        $profile->rating += 15;
+        $profile->save();
+    }
+
+    public function updateLoser(Profile $profile): void
+    {
+        $profile->game_total++;
+        $profile->lose_total++;
+        $profile->rating -= 15;
+        $profile->save();
+    }
+
+    public function updateDrawer(Profile $profile): void
+    {
+        $profile->game_total++;
+        $profile->draw_total++;
+        $profile->rating += 3;
+        $profile->save();
     }
 }
