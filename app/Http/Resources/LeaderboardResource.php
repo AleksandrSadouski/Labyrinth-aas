6a1dcb0537e257c4a56243a26cd9a099bf51edf1
AleksandrSadouski <sashasadovskyi020507@gmail.com>
2026-08-28 13:34:51 +0300

    fix: fix LeaderboardResource

--- app/Http/Resources/LeaderboardResource.php
+++ app/Http/Resources/LeaderboardResource.php
@@ -12,6 +12,7 @@ class LeaderboardResource extends JsonResource
     {
         return [
         'name' => $this->name,
-        'max_rating' => $this->rating];
+        'max_rating' => $this->when(!empty($this->rating), $this->rating),
+        'max_sp_game_completed' => $this->when(!empty($this->sp_game_completed), $this->sp_game_completed)];
     }
 }
